b2c3b3c30676b3b7bc47f166d9894354b2419b78
Partha <parthabiswas2024@gmail.com>
2022-04-24 11:10:16 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {useQuery} from 'react-query';
+// components 
+import { Container, GridJustification, Button, Typography, Card, CardContent, CardActions, CardActionArea} from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
+import {makeStyles} from '@material-ui/core/styles'
+import { Grid } from '@material-ui/core';
+// styles
+import { Wrapper } from './App.style';
+//types
+// export type CartItemType = {
+//   id: number;
+//   email: string;
+//   first_name: string;
+//   last_name: string;
+//   avatar: string;
+// };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// type Props = {
+//   item: CartItemType,
+//   handleAddToCart: (clickedItem: CartItemType) => void;
+// }
 
-export default App;
+const useStyles = makeStyles((theme) =>({
+  root: {
+    minWidth: '100%',   
+    minHeight: '100%',    
+    paddingTop: theme.spacing(),
+  },
+  card:{
+    backgroundColor:theme.palette.grey[200]
+  },
+  grid:{
+    width:"100vw",
+    marginLeft:"0"
+  }
+}));
+  
+  export default function App() {
+    const classes = useStyles();
+    const [users, setUsers] = React.useState([]);
+    const f = async () => {
+      const res = await fetch("https://reqres.in/api/users/");
+      const json = await res.json();
+      setUsers(json.data);
+    };
+    React.useEffect(() => {
+      f();
+    }, []);
+    return (
+      <Container className={classes.root}>
+        <Grid container spacing={5}>
+          {users.length &&
+            users.map((user) => {
+              return (
+                <Grid item sm={3}>{/* card  */}
+                  <Card className={classes.card}>
+                    <CardActionArea>
+                      <CardMedia component="img" image={user.avatar}   />
+                        <CardContent>
+                            <Typography variant='h5'>{user.first_name} {user.last_name}</Typography>
+                            <Typography variant='subtitle1'>ID : {user.id}</Typography>
+                            <Typography variant='subtitle1'>Email ID : {user.email}</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>
+                  </Grid>
+              );
+            })}
+        </Grid>
+      </Container>
+    );
+  }
+
+
+    
